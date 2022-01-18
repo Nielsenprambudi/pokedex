2a1aa32d48b0433e96330bbacd841fecbc670d5f
Nielsenprambudi <nielsenprambudiasallie@gmail.com>
2022-01-19 01:08:23 +0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery,gql } from '@apollo/client';
+import { Provider } from 'react-redux';
+import store from './store/store';
+// import {jsx} from '@emotion/react';
+
+const client = new ApolloClient({
+  uri: 'https://graphql-pokeapi.graphcdn.app/',
+  cache: new InMemoryCache()
+});
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Pokemon, Gotta Catch'em All</title>
+        <meta
+          name="description"
+          content="Pokedex for data to hunt the pokemon"
+        />
+      </Head>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </ApolloProvider>
+    </Fragment>
+  )
 }
 
 export default MyApp
