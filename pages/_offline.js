73c72f31666163de7c34606e3e8443c0bf4042cf
Nielsenprambudi <nielsenprambudiasallie@gmail.com>
2022-04-27 23:36:43 +0700

    Update _offline.js

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 
-export default () => (
+export default offlinePage = () => (
     <div>
         <Head>
             <title>pokedex is offline</title>
         </Head>
         <h1 style={{textAlign: 'center'}}>
-            I'm sorry, it looks like you are in offline mode please check your internet connection or your flight mode
+            I am sorry, it looks like you are in offline mode please check your internet connection or your flight mode
         </h1>
 
     </div>
